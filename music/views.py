549a10e3c36bb7da0ea2a1ac7244b3a06d537107
saber <saber@mail.com>
2018-12-24 13:07:17 +0200

    episode 14, creating main page with all albums, main page of all albms, linked to each album detail

--- music/views.py
+++ music/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django import http
+from django.http import HttpResponse
 from django.urls import include
 from .models import *
-999
+from django.template import loader, Template, Context
 
 def index(request):
+    all = Album.objects.all()
+    cont = {"albums": all }
+    temp_load = loader.get_template('music/index.html')
+    temp_rendering = temp_load.render(cont, request)
+    fw = open('debug.txt', 'w')
+    fw.write(str(temp_rendering))
+    fw.close()
+    return  HttpResponse(temp_rendering)
+
+def index1(request):
     all = Album.objects.all()
     html= '<html><head><title> Saber developer as  title </title></head><body>'
     for album in all:
@@ -16,10 +26,10 @@ def index(request):
     html += ' </body></html>'
 
 
-    return http.HttpResponse(html)
+    return HttpResponse(html)
 
 def detail(request, album_id):
-    return http.HttpResponse('<html><head><title> Saber developer as  title </title></head><body><h1>Hello its Me saber DEVELOPER</h1><h2>Here are the details of Album: ' + str(album_id) + '</h2></body></html>')
+    return HttpResponse('<html><head><title> Saber developer as  title </title></head><body><h1>Hello its Me saber DEVELOPER</h1><h2>Here are the details of Album: ' + str(album_id) + '</h2></body></html>')
 
 def title(request, album_title):
-    return http.HttpResponse('<html><head><title> Saber developer as  title </title></head><body><h1>Hello its Me saber DEVELOPER</h1><h2>Here are the details of Album title: ' + str(album_title) + '</h2></body></html>')
+    return HttpResponse('<html><head><title> Saber developer as  title </title></head><body><h1>Hello its Me saber DEVELOPER</h1><h2>Here are the details of Album title: ' + str(album_title) + '</h2></body></html>')
